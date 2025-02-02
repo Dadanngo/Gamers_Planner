2a55dacaeda4e3b428be5d21e3e72f79b49a9dab
Dadanngo <daichi72221189@gmail.com>
2025-02-03 03:14:27 +0900

    トップページ仮実装

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,6 @@
+class TopController < ApplicationController
+
+  def index
+      @message = "Hello,Rails!"
+  end
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,27 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ['M PLUS 1p', 'serif',]
+      },
+      animation: {
+        "scale-up-hor-left": "scale-up-hor-left 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) both"
+      },
+      keyframes: {
+        "scale-up-hor-left": {
+          "0%": {
+            transform: "scaleX(.4)",
+            "transform-origin": "0% 0%"
+          },
+          to: {
+            transform: "scaleX(1)",
+            "transform-origin": "0% 0%"
+          }
+        }
+      }
+    }
+  }
 }
