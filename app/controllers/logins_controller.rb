80e768fa3b6185a7c722e35a913fa8100afa90ce
Dadanngo <daichi72221189@gmail.com>
2025-02-09 03:42:04 +0900

    feat #8 ログイン機能実装

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,4 +1,17 @@
 class LoginsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate_password(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "ログインしました"
+      redirect_to users_path
+    else
+      flash.now[:warning] = 'ログインできませんでした'
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
