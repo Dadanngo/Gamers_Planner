35ee22868519b834256507ad7e917b67d3f57e7a
Dadanngo <daichi72221189@gmail.com>
2025-02-10 23:36:52 +0900

    test: #8 テストコード編集

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -6,10 +6,7 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
     user = User.new(email: 'test@example.com', password: password)
     user.save
 
-    visit new_login_url
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: password
-    click_on "保存"
-    assert_text "さん"
+    get profile_url(user)
+    assert_response :success
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should create user" do
+  test "ユーザー作成" do
     assert_difference("User.count") do
       post users_url, params: { user: { email: 'test@example.com', name: 'test_user', password: 'password123', password_confirmation: 'password123' } }
     end
-    assert_not_nil @user.errors.full_messages
-    assert_redirected_to profile_url(@user)
+    assert_redirected_to profile_url(User.last)
   end
 end
