1f5b8d6ee8e68558783be24fcfbc291eb1a0ea0c
Dadanngo <daichi72221189@gmail.com>
2025-02-10 23:20:52 +0900

    test: #8 テストコード不要アクション編集削除

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get profiles_show_url
-    assert_response :success
+  test "マイページ表示" do
+    password = "password123"
+    user = User.new(email: 'test@example.com', password: password)
+    user.save
+
+    visit new_login_url
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: password
+    click_on "保存"
+    assert_text "さん"
   end
 end
