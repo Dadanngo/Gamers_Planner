13437d3bf6f6e19674ceed92aa79e3aa0c4a72a8
Dadanngo <daichi72221189@gmail.com>
2025-02-10 23:49:28 +0900

    test: #8 テストコード編集_2

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,10 +2,8 @@ require "test_helper"
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
   test "マイページ表示" do
-    password = "password123"
-    user = User.new(email: 'test@example.com', password: password)
-    user.save
-
+    user = User.create(email: 'test@example.com', name: 'test_user', password: 'password123', password_confirmation: 'password123')
+    post login_path, params: { email: user.email, password: 'password123' }
     get profile_url(user)
     assert_response :success
   end
