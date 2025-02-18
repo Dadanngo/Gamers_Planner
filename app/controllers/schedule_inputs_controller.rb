9e8b360441662a2b39da5067f292cd1dea111b0c
Dadanngo <daichi72221189@gmail.com>
2025-02-18 20:35:41 +0900

    feat: 集約用予定表実装(closes #24,closes #70)

--- app/controllers/schedule_inputs_controller.rb
+++ app/controllers/schedule_inputs_controller.rb
@@ -1,13 +1,16 @@
 class ScheduleInputsController < ApplicationController
     before_action :set_event
-    before_action :set_schedule_input, only: [ :index ]
-
+  
     def new
-      @schedule_input = @event.schedule_inputs.new
-      # 各 event_time の ID をキーにした初期データをセット
-      @schedule_input.response = @event.event_times.each_with_object({}) do |event_time, hash|
-       hash[event_time.id.to_s] = nil
-       end.to_json
+      @schedule_input = @event.schedule_inputs.find_by(token: params[:token])
+      
+      if @schedule_input.nil?
+        @schedule_input = @event.schedule_inputs.new
+        @schedule_input.token = SecureRandom.hex(16)
+        @schedule_input.response = @event.event_times.each_with_object({}) { |event_time, hash| hash[event_time.id.to_s] = nil }.to_json
+      else
+        @schedule_input = @event.schedule_inputs.new
+      end
     end
 
     def create
@@ -17,16 +20,20 @@ class ScheduleInputsController < ApplicationController
       # JSONに変換
       @schedule_input.response = schedule_input_params[:response].to_json
       @schedule_input.event_time_id = schedule_input_params[:event_time_id].values.first.to_i if schedule_input_params[:event_time_id].present?
+      
       if @schedule_input.save
         redirect_to event_schedule_inputs_path(@event), notice: '登録完了しました'
       else
+        flash[:alert] = "登録に失敗しました。入力内容を確認してください"
         render :new
       end
     end
 
     def index
-      @schedule_inputs = @event.schedule_inputs
+      @schedule_inputs = @event.schedule_inputs.includes(:event_time) if @event
     end
+
+    
 
   private
 
@@ -35,10 +42,14 @@ class ScheduleInputsController < ApplicationController
   end
 
   def set_event
-  @event = Event.find(params[:event_id])
-  rescue ActiveRecord::RecordNotFound
-  flash[:alert] = "イベントが見つかりません。"
-  redirect_to root_path
+    if params[:event_id].present?
+      @event = Event.find_by(id: params[:event_id])
+    end
+  
+    unless @event
+      flash[:alert] = "イベントが見つかりません。"
+      redirect_to root_path
+    end
   end
 
   def set_schedule_input
