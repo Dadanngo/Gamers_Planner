cb84f6fd7665aa0a2bd7d46ccdacaccf5aae181f
Dadanngo <daichi72221189@gmail.com>
2025-02-19 16:48:28 +0900

    feat: 入力情報変更機能実装(close #72 ,tweak #44)

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'manual/show', to: 'manual#show', as: 'manual_show'
-  get '/events/:event_id/schedule_inputs/:token', to: 'schedule_inputs#show', as: 'event_schedule_input'
+  get '/events/url/:url/schedule_inputs', to: 'schedule_inputs#index', as: 'event_schedule_inputs_by_url'
+  get '/events/url/:url/schedule_inputs/:token/edit', to: 'schedule_inputs#edit', as: 'edit_event_schedule_input_by_url'
+  patch '/events/url/:url/schedule_inputs/:token', to: 'schedule_inputs#update', as: 'update_event_schedule_input_by_url'
+  delete '/events/url/:url/schedule_inputs/:token', to: 'schedule_inputs#destroy', as: 'delete_event_schedule_input_by_url'
   get '/events/url/:url', to: 'events#show', as: 'event_by_url'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
