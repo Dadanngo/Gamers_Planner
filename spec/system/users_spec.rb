96774548040daf4bcc3acec4842c57aeaf7519bd
Dadanngo <daichi72221189@gmail.com>
2025-05-01 22:29:26 +0900

    テストコード編集

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -75,6 +75,7 @@ RSpec.describe "Users", type: :system do
       fill_in "メールアドレス", with: user.email
       fill_in "パスワード", with: "password"
       click_button "ログイン"
+      expect(page).to have_current_path(root_path, wait: 5)
       expect(page).to have_content("ログインしました")
     end
 
